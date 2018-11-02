d6c34bd83662ca4964c604b6a14f9ba687cb3095
Miguel Angel <manaranjoc@alcazares.edu.co>
2018-11-01 23:13:32 -0500

    Commit inicial Ionic

--- src/pages/tabs/tabs.ts
+++ src/pages/tabs/tabs.ts
@@ -10,8 +10,8 @@ import { HomePage } from '../home/home';
 export class TabsPage {
 
   tab1Root = HomePage;
-  tab2Root = AboutPage;
-  tab3Root = ContactPage;
+  tab2Root = HomePage;
+  tab3Root = HomePage;
 
   constructor() {
 
